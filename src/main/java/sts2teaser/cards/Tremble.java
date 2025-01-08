c9e0939c627611bce94e0eeac5301ec5df73aeaf
CoomSalad <chanlokfung816@gmail.com>
2025-01-08 09:53:46 +0800

    Create Tremble Card

---
+++ src/main/java/sts2teaser/cards/Tremble.java
@@ -0,0 +1,63 @@
+package sts2teaser.cards;
+
+import basemod.abstracts.CustomCard;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.IntimidateEffect;
+
+import static sts2teaser.STS2Teaser.*;
+
+public class Tremble extends CustomCard {
+    public final static String ID = makeID("Tremble");
+
+    private static String NAME = "Tremble";
+    private static String DESC = "Apply !M! Vulnerable. NL Exhaust.";
+    private static String UPGRADED_DESC = "Apply !M! Vulnerable to ALL enemies. NL Exhaust.";
+
+    public Tremble() {
+        super(ID, NAME, makeCardPath("Tremble"), 0, DESC, AbstractCard.CardType.SKILL, AbstractCard.CardColor.RED, AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 2;
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot((AbstractGameAction)new SFXAction("INTIMIDATE"));
+        addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new IntimidateEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY), 1.0F));
+        if (upgraded) {
+            for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+                addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new VulnerablePower((AbstractCreature)mo, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+            }
+        } else {
+            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)m, (AbstractCreature)p, (AbstractPower)new VulnerablePower((AbstractCreature)m, this.magicNumber, false), this.magicNumber));
+        }
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            rawDescription = UPGRADED_DESC;
+            target = AbstractCard.CardTarget.ALL_ENEMY;
+        }
+    }
+
+    public AbstractCard makeCopy() {
+        return new Tremble();
+    }
+}
