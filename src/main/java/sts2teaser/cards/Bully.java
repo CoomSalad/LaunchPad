6be25f8d121d3e6fb9d0d671cf5f71b9212e5f14
CoomSalad <chanlokfung816@gmail.com>
2025-01-08 21:28:12 +0800

    Create Bully Card

---
+++ src/main/java/sts2teaser/cards/Bully.java
@@ -0,0 +1,81 @@
+package sts2teaser.cards;
+
+import basemod.abstracts.CustomCard;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Iterator;
+
+import static sts2teaser.STS2Teaser.*;
+
+public class Bully extends CustomCard {
+    public final static String ID = makeID("Bully");
+
+    private static String NAME = "Bully";
+    private static String DESC = "Deal !D! damage. NL Deal !M! additional damage for each Vulnerable on the enemy.";
+
+    public Bully() {
+        super(ID, NAME, makeCardPath("Bully"), 0, DESC, AbstractCard.CardType.ATTACK, AbstractCard.CardColor.RED, CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY);
+        baseDamage = 4;
+        baseMagicNumber = magicNumber = 2;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot((AbstractGameAction)new DamageAction((AbstractCreature)m, new DamageInfo((AbstractCreature)p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+    }
+
+    public void applyPowers() {
+        int realBaseDamage = baseDamage;
+        int targetMonsterVulnerable = 0;
+        Iterator var3 = AbstractDungeon.getCurrRoom().monsters.monsters.iterator();
+        while(var3.hasNext()) {
+            AbstractMonster m = (AbstractMonster)var3.next();
+            m.hb.update();
+            if (m.hb.hovered && !m.isDying && !m.isEscaping && m.currentHealth > 0) {
+                if (m.hasPower("Vulnerable")) {
+                    targetMonsterVulnerable = m.getPower("Vulnerable").amount;
+                }
+                break;
+            }
+        }
+        baseDamage += targetMonsterVulnerable * baseMagicNumber;
+        super.applyPowers();
+        baseDamage = realBaseDamage;
+        isDamageModified = (damage != baseDamage);
+    }
+
+    public void calculateCardDamage(AbstractMonster mo) {
+        int targetMonsterVulnerable = 0;
+        if (mo.hasPower("Vulnerable")) {
+            targetMonsterVulnerable = mo.getPower("Vulnerable").amount;
+        }
+        int realBaseDamage = baseDamage;
+        baseDamage += targetMonsterVulnerable * baseMagicNumber;
+        super.calculateCardDamage(mo);
+        baseDamage = realBaseDamage;
+        isDamageModified = (damage != baseDamage);
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeDamage(1);
+            upgradeMagicNumber(1);
+        }
+    }
+
+    public AbstractCard makeCopy() {
+        return new Bully();
+    }
+}
